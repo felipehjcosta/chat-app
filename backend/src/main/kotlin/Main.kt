aa914b91ac6cf1e1b81b4bcd6b473a0c163704ae
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-02-14 21:38:28 -0200

    Use socket.io as the chat implementation on backend

--- backend/src/main/kotlin/Main.kt
+++ backend/src/main/kotlin/Main.kt
@@ -1,17 +1,30 @@
 external fun require(module: String): dynamic
 
 fun main(args: Array<String>) {
-    println("Hello JavaScript!")
+    println("Hello NodeJS!")
 
     val express = require("express")
     val app = express()
+
+    val server = app.listen(8080, {
+        println("Server is running on port 8080")
+    })
+
+    val socket = require("socket.io")
+    val io = socket(server)
+
+    io.on("connection") { socket ->
+        println("socket ID: ${socket.id}")
+
+        socket.on("SEND_MESSAGE") { data ->
+            println("SEND_MESSAGE: ${data}")
+            io.emit("RECEIVE_MESSAGE", data)
+        }
+    }
 
     app.get("/", { req, res ->
         res.type("text/plain")
         res.send("Hello World Kotlin JS!")
     })
 
-    app.listen(3000, {
-        println("Listening on port 3000")
-    })
 }
