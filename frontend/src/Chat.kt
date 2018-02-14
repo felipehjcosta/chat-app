41861413c32ce71ac7e1612dd3ec22633b0ef0e4
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-02-14 18:01:15 -0200

    Create a simple send/receive chat logic, but without backend yet
    I find out something weird: Why couldn't I access value of a event inside setState closure??

--- frontend/src/Chat.kt
+++ frontend/src/Chat.kt
@@ -1,11 +1,38 @@
 import kotlinx.html.InputType
-import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
+import kotlinx.html.js.onChangeFunction
+import kotlinx.html.js.onClickFunction
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.events.Event
+import react.*
 import react.dom.*
 
-class Chat : RComponent<RProps, RState>() {
+class Chat : RComponent<RProps, Chat.State>() {
+
+    private val sendButtonHandler: (Event) -> Unit = {
+        it.preventDefault()
+        sendMessage()
+
+        setState {
+            message = ""
+        }
+    }
+
+    init {
+        state = State()
+
+    }
+
+    private fun sendMessage() {
+        receiveMessage(mapOf("author" to state.username, "message" to state.message))
+    }
+
+    private fun receiveMessage(data: Map<String, String>) {
+        console.log(">>> Message '${data["message"]}' from '${data["author"]}' received")
+        setState {
+            messages = messages.toMutableList().apply { add(data["author"]!! to data["message"]!!) }
+        }
+    }
+
     override fun RBuilder.render() {
         div(classes = "container") {
             div(classes = "row") {
@@ -15,22 +42,50 @@ class Chat : RComponent<RProps, RState>() {
                             div(classes = "card-title") { +"Global Chat" }
                             hr {}
                             div(classes = "messages") {
-
+                                state.messages.map {
+                                    div {
+                                        +"${it.first}: ${it.second}"
+                                    }
+                                }
                             }
 
                             div(classes = "card-footer") {
                                 input(type = InputType.text,
                                         classes = "form-control") {
-                                    attrs { placeholder = "Username" }
+                                    attrs {
+                                        placeholder = "Username"
+                                        defaultValue = state.username
+                                        onChangeFunction = {
+
+                                            val value = it.target.unsafeCast<HTMLInputElement>().value
+                                            setState {
+                                                username = value
+                                            }
+                                        }
+                                    }
                                 }
                                 br {}
                                 input(type = InputType.text,
                                         classes = "form-control") {
-                                    attrs { placeholder = "Message" }
+                                    attrs {
+                                        placeholder = "Message"
+                                        defaultValue = state.message
+                                        onChangeFunction = {
+                                            val value = it.target.unsafeCast<HTMLInputElement>().value
+                                            setState {
+                                                message = value
+                                            }
+                                        }
+                                    }
                                 }
                                 br {}
                                 button(classes = "btn btn-primary form-control") {
                                     +"Send"
+
+                                    attrs {
+                                        onClickFunction = sendButtonHandler
+                                    }
+
                                 }
                             }
                         }
@@ -39,6 +94,10 @@ class Chat : RComponent<RProps, RState>() {
             }
         }
     }
+
+    class State(var username: String = "",
+                var message: String = "",
+                var messages: List<Pair<String, String>> = emptyList()) : RState
 }
 
 fun RBuilder.chat() = child(Chat::class) {}
