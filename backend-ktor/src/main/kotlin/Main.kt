c47d46ec13ca002ea91bb772792691c4fe74d534
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-11-04 11:44:02 -0200

    Create another backed module using ktor
    This will replace the node backend in the future

---
+++ backend-ktor/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }.start(wait = true)
+}
