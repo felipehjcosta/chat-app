c556e24d942b764f2926b4a3187c0d84951d8c87
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-11-04 22:26:38 -0200

    Implement a simple chat logic using ktor websocket feature

--- backend-ktor/src/main/kotlin/Main.kt
+++ backend-ktor/src/main/kotlin/Main.kt
@@ -1,19 +1,42 @@
 import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.http.ContentType
+import io.ktor.http.cio.websocket.DefaultWebSocketSession
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.readText
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.websocket.WebSockets
+import io.ktor.websocket.webSocket
+import java.util.*
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
+        install(WebSockets)
         routing {
+
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                call.respondText("Hello World server!", ContentType.Text.Plain)
             }
-            get("/demo") {
-                call.respondText("HELLO WORLD!")
+            val wsConnections = Collections.synchronizedSet(LinkedHashSet<DefaultWebSocketSession>())
+
+            webSocket("/chat") {
+                wsConnections += this
+                try {
+                    while (true) {
+                        when (val frame = incoming.receive()) {
+                            is Frame.Text -> {
+                                val text = frame.readText()
+                                wsConnections.forEach { it.outgoing.send(Frame.Text(text)) }
+                            }
+                        }
+                    }
+                } finally {
+                    wsConnections -= this
+                }
             }
         }
     }.start(wait = true)
