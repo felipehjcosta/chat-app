72424280e275bffcc8709127629aa777fdbbb4e9
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-11-11 13:06:10 -0200

    Implement chat client on JVM with OkHttp

--- client/common-client-jvm/src/main/kotlin/ChatClient.kt
+++ client/common-client-jvm/src/main/kotlin/ChatClient.kt
@@ -1,9 +1,36 @@
-actual class ChatClient actual constructor(url: String) {
+import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.json.JSON
+import kotlinx.serialization.serializer
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+
+actual class ChatClient actual constructor(url: String) : WebSocketListener() {
+
+    private val websocketClient: WebSocket
+
+    private var receiveMessage: ((Message) -> Unit)? = null
+
+    init {
+        val okHttpClient = OkHttpClient.Builder().build()
+        val request = Request.Builder().url(url).build()
+
+        websocketClient = okHttpClient.newWebSocket(request, this)
+    }
+
+    @ImplicitReflectionSerializer
     actual fun send(message: Message) {
-        TODO("NOT Implemented")
+        websocketClient.send(JSON.stringify(Message::class.serializer(), message))
     }
 
     actual fun receive(receiveBlock: (Message) -> Unit) {
-        TODO("NOT Implemented")
+        receiveMessage = receiveBlock
+    }
+
+    @ImplicitReflectionSerializer
+    override fun onMessage(webSocket: WebSocket, text: String) {
+        super.onMessage(webSocket, text)
+        receiveMessage?.let { it(JSON.parse(Message::class.serializer(), text)) }
     }
 }
