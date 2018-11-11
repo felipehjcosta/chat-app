963e5f2ce0e2e4ddad2c9c7551d1529c78c6a2ed
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-11-11 15:12:11 -0200

    Implement chat logic on Android

--- android/src/main/java/com/example/android/MainActivity.kt
+++ android/src/main/java/com/example/android/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.example.android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.github.felipehjcosta.chatapp.Message
+import com.github.felipehjcosta.chatapp.client.ChatClient
+import com.github.felipehjcosta.recyclerviewdsl.onRecyclerView
+import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.android.synthetic.main.activity_main.message_input as messageInput
+import kotlinx.android.synthetic.main.activity_main.messages_recycler_view as messagesRecyclerView
+import kotlinx.android.synthetic.main.activity_main.send_button as sendButton
+import kotlinx.android.synthetic.main.activity_main.username_input as usernameInput
 
+@ImplicitReflectionSerializer
 class MainActivity : AppCompatActivity() {
+
+    private val chatClient = ChatClient("ws://192.168.1.6:8080/chat")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupRecyclerView()
+
+        chatClient.receive { runOnUiThread { receiveMessage(it) } }
+
+        sendButton.setOnClickListener { sendMessage() }
+    }
+
+    private fun setupRecyclerView() {
+        onRecyclerView(messagesRecyclerView) {
+            withLinearLayout()
+            bind(android.R.layout.simple_list_item_1) {
+                withItems(emptyList<Message>()) {
+                    on<TextView>(android.R.id.text1) {
+                        it.view?.text = "${it.item?.author}: ${it.item?.message}"
+                    }
+                }
+            }
+        }
+    }
+
+    private fun sendMessage() {
+        val author = usernameInput.text.toString()
+        val message = messageInput.text.toString()
+        chatClient.send(Message(author, message))
+        usernameInput.text?.clear()
+        messageInput.text?.clear()
+    }
+
+    private fun receiveMessage(message: Message) {
+        onRecyclerView(messagesRecyclerView) {
+            bind(android.R.layout.simple_list_item_1) {
+                addExtraItems(listOf(message))
+            }
+        }
     }
 }
