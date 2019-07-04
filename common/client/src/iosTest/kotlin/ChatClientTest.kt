736a6a0c4eb08dc8f76145015b431a313ad92c4c
Felipe Costa <felipe.h.j.costa@gmail.com>
2019-07-04 01:42:18 -0300

    Setup ChatClient test

---
+++ common/client/src/iosTest/kotlin/ChatClientTest.kt
@@ -0,0 +1,15 @@
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class ChatClientTest {
+
+    @BeforeTest
+    fun setUp() {
+    }
+
+    @Test
+    fun ensureAStartedConnectionIsOpenedByClient() {
+        assertTrue { true }
+    }
+}
