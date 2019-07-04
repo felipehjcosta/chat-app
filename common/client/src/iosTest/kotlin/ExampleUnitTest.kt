a2b81f6622fd494e85933c245ae252e86a2d283e
Felipe Costa <felipe.h.j.costa@gmail.com>
2019-07-04 01:39:38 -0300

    Add example unit test to iosTest

---
+++ common/client/src/iosTest/kotlin/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.github.felipehjcosta.chatapp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
