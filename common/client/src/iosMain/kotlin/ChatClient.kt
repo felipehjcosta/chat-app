2919a7e474ce71e5d719c0b4ded6df40c034896d
Felipe Costa <felipe.h.j.costa@gmail.com>
2019-07-14 11:41:23 -0300

    Add chat implementation for iOS

--- common/client/src/iosMain/kotlin/ChatClient.kt
+++ common/client/src/iosMain/kotlin/ChatClient.kt
@@ -1,24 +1,47 @@
 package com.github.felipehjcosta.chatapp.client
 
+import cocoapods.SocketRocket.SRWebSocket
+import cocoapods.SocketRocket.SRWebSocketDelegateProtocol
 import com.github.felipehjcosta.chatapp.Message
 import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.json.Json
+import platform.Foundation.NSURL
+import platform.darwin.NSObject
 
 @UseExperimental(ImplicitReflectionSerializer::class)
 internal actual open class ChatClient actual constructor(private val url: String) {
 
+    private var socket: SRWebSocket? = null
+
+    private var receiveMessage: ((Message) -> Unit)? = null
+
+    private var failure: ((Throwable) -> Unit)? = null
+
+    private val webSocketDelegate = WebSocketDelegate()
+
     actual open fun start() {
-        TODO()
+        socket = SRWebSocket(NSURL(string = url)).apply {
+            delegate = webSocketDelegate
+        }.also {
+            it.open()
+        }
     }
 
     actual open fun send(message: Message) {
-        TODO()
+        socket?.send(Json.stringify(Message.serializer(), message))
     }
 
     actual open fun receive(receiveBlock: (Message) -> Unit) {
-        TODO()
+        receiveMessage = receiveBlock
     }
 
     actual open fun onFailure(throwableBlock: (Throwable) -> Unit) {
-        TODO()
+        failure = throwableBlock
+    }
+
+    inner class WebSocketDelegate : NSObject(), SRWebSocketDelegateProtocol {
+        override fun webSocket(webSocket: SRWebSocket?, didReceiveMessage: Any?) {
+            receiveMessage?.invoke(Json.parse(Message.serializer(), didReceiveMessage.toString()))
+        }
     }
 }
