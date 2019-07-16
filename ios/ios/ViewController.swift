3e9e86804b10b2e223b83765adc8f100e6f9574a
Felipe Costa <felipe.h.j.costa@gmail.com>
2019-07-16 17:22:02 -0300

    Add chat UI for iOS

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -9,27 +9,54 @@
 import UIKit
 import client
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    var chatViewModel: ChatViewModel?
+    var messages: [CoreMessage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        let chatViewModel = ChatInjector.init().createViewModel()
-        chatViewModel.start()
-        
-        sleep(5)
-        chatViewModel.sendMessage(message: CoreMessage(author: "test", message: "message"))
+        chatViewModel = ChatInjector.init().createViewModel()
+        chatViewModel?.start()
 
-        chatViewModel.onChat = { (message) -> KotlinUnit in
+        chatViewModel?.onChat = { (message) -> KotlinUnit in
             print(">>> messge: \(message)")
+            self.messages.append(message)
+            let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+            self.collectionView.insertItems(at: [indexPath])
             return KotlinUnit()
         }
         
-        chatViewModel.showFailureMessage = { (showFailureMessage) -> KotlinUnit in
+        chatViewModel?.showFailureMessage = { (showFailureMessage) -> KotlinUnit in
             print(">>> showFailureMessage: \(showFailureMessage)")
             return KotlinUnit()
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! CollectionViewCell
+        
+        cell.messageLabel.text = "\(messages[indexPath.row].author): \(messages[indexPath.row].message)"
+        
+        return cell
+    }
+    @IBAction func sendAction(_ sender: Any) {
+        
+        let author = usernameTextField.text ?? ""
+        let message = messageTextField.text ?? ""
+        chatViewModel?.sendMessage(message: CoreMessage(author: author, message: message))
+        usernameTextField.text = nil
+        messageTextField.text = nil
+    }
 }
-
