ad1c5b40910407a49a9db5c75c100d7bfd444fb2
felipehjcosta <felipe.h.j.costa@gmail.com>
2019-09-27 22:40:39 -0300

    Bump CI Xcode to 11

--- build-native-ios.sh
+++ build-native-ios.sh
@@ -25,4 +25,4 @@ fi
 pod --project-directory="$IOS_DIR" install
 
 # Run test with Xcode
-xcodebuild -workspace "$IOS_DIR/ios.xcworkspace" -scheme ios -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone Xʀ,OS=12.4"
+xcodebuild -workspace "$IOS_DIR/ios.xcworkspace" -scheme ios -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11,OS=13.0"
