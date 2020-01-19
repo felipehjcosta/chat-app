85e327e873605690f07728908b8991b64303d82e
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-01-19 09:26:39 -0300

    Move android chat UI to fragment

---
+++ android/src/main/java/com/example/android/ChatListFragment.kt
@@ -0,0 +1,88 @@
+package com.example.android
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.github.felipehjcosta.chatapp.Message
+import com.github.felipehjcosta.chatapp.client.ChatInjector
+import com.github.felipehjcosta.chatapp.client.ChatViewModel
+import com.github.felipehjcosta.recyclerviewdsl.onRecyclerView
+import kotlinx.android.synthetic.main.chat_list_fragment.message_input as messageInput
+import kotlinx.android.synthetic.main.chat_list_fragment.messages_recycler_view as messagesRecyclerView
+import kotlinx.android.synthetic.main.chat_list_fragment.send_button as sendButton
+import kotlinx.android.synthetic.main.chat_list_fragment.username_input as usernameInput
+
+class ChatListFragment : Fragment() {
+
+    private val chatViewModel: ChatViewModel by ChatInjector.viewModel()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.chat_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+
+        chatViewModel.onChat = { activity?.runOnUiThread { receiveMessage(it) } }
+
+        sendButton.setOnClickListener { sendMessage() }
+        messageInput.setOnEditorActionListener { _, actionId, _ ->
+            return@setOnEditorActionListener when (actionId) {
+                EditorInfo.IME_ACTION_SEND -> {
+                    sendMessage()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        chatViewModel.start()
+    }
+
+    private fun setupRecyclerView() {
+        onRecyclerView(messagesRecyclerView) {
+            withLinearLayout()
+            bind(android.R.layout.simple_list_item_1) {
+                withItems(emptyList<Message>()) {
+                    on<TextView>(android.R.id.text1) {
+                        it.view?.text = "${it.item?.author}: ${it.item?.message}"
+                    }
+                }
+            }
+        }
+    }
+
+    private fun sendMessage() {
+        hideKeyboard()
+        val author = usernameInput.text.toString()
+        val message = messageInput.text.toString()
+        chatViewModel.sendMessage(Message(author, message))
+        usernameInput.text?.clear()
+        messageInput.text?.clear()
+    }
+
+    private fun receiveMessage(message: Message) {
+        onRecyclerView(messagesRecyclerView) {
+            bind(android.R.layout.simple_list_item_1) {
+                addExtraItems(listOf(message))
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        view?.findViewById<View>(android.R.id.content)?.run {
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager?
+            imm?.hideSoftInputFromWindow(windowToken, 0)
+        }
+    }
+}
--- android/src/main/java/com/example/android/MainActivity.kt
+++ android/src/main/java/com/example/android/MainActivity.kt
@@ -1,82 +1,12 @@
 package com.example.android
 
-import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.github.felipehjcosta.chatapp.Message
-import com.github.felipehjcosta.chatapp.client.ChatInjector
-import com.github.felipehjcosta.chatapp.client.ChatViewModel
-import com.github.felipehjcosta.recyclerviewdsl.onRecyclerView
-import kotlinx.android.synthetic.main.activity_main.message_input as messageInput
-import kotlinx.android.synthetic.main.activity_main.messages_recycler_view as messagesRecyclerView
-import kotlinx.android.synthetic.main.activity_main.send_button as sendButton
-import kotlinx.android.synthetic.main.activity_main.username_input as usernameInput
-
 
 class MainActivity : AppCompatActivity() {
-
-    private val chatViewModel: ChatViewModel by ChatInjector.viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setupRecyclerView()
-
-        chatViewModel.onChat = { runOnUiThread { receiveMessage(it) } }
-
-        sendButton.setOnClickListener { sendMessage() }
-        messageInput.setOnEditorActionListener { _, actionId, _ ->
-            return@setOnEditorActionListener when (actionId) {
-                EditorInfo.IME_ACTION_SEND -> {
-                    sendMessage()
-                    true
-                }
-                else -> false
-            }
-        }
-
-        chatViewModel.start()
-    }
-
-    private fun setupRecyclerView() {
-        onRecyclerView(messagesRecyclerView) {
-            withLinearLayout()
-            bind(android.R.layout.simple_list_item_1) {
-                withItems(emptyList<Message>()) {
-                    on<TextView>(android.R.id.text1) {
-                        it.view?.text = "${it.item?.author}: ${it.item?.message}"
-                    }
-                }
-            }
-        }
-    }
-
-    private fun sendMessage() {
-        hideKeyboard()
-        val author = usernameInput.text.toString()
-        val message = messageInput.text.toString()
-        chatViewModel.sendMessage(Message(author, message))
-        usernameInput.text?.clear()
-        messageInput.text?.clear()
-    }
-
-    private fun receiveMessage(message: Message) {
-        onRecyclerView(messagesRecyclerView) {
-            bind(android.R.layout.simple_list_item_1) {
-                addExtraItems(listOf(message))
-            }
-        }
-    }
-
-    private fun hideKeyboard() {
-        findViewById<View>(android.R.id.content)?.run {
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager?
-            imm?.hideSoftInputFromWindow(windowToken, 0)
-        }
     }
 }
