0231f58e81b494466358783d7dbcc00be76f7657
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-01-19 10:55:38 -0300

    Update Toolbar according to navigation

--- android/src/main/java/com/example/android/MainActivity.kt
+++ android/src/main/java/com/example/android/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.navHostFragment).navigateUp()
