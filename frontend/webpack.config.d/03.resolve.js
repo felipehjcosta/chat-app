ae0df51573d24aba2686a44084ba6819b7a2a4bb
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-01-23 08:36:18 -0300

    [Frontend] Remove hack for Kotlin JS

--- frontend/webpack.config.d/03.resolve.js
+++
@@ -1,7 +0,0 @@
-// workaround for https://youtrack.jetbrains.com/issue/KT-31807
-config.resolve.alias = {
-    'kotlin-extensions': '@jetbrains/kotlin-extensions',
-    'kotlin-react': '@jetbrains/kotlin-react',
-    'kotlin-react-dom': '@jetbrains/kotlin-react-dom',
-    'kotlinx-html-js': 'kotlinx-html',
-};
