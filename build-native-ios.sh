951b492f8ee47f35d54039ec191510109e29b212
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-01-24 19:17:23 -0300

    Remove cocoapods code from build script

--- build-native-ios.sh
+++ build-native-ios.sh
@@ -7,22 +7,8 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
 KOTLIN_DIR="$DIR"
 IOS_DIR="$DIR/ios"
 
-#Check CocoaPods version.
-REQUIRED_POD_VERSION="1.6.1"
-POD_VERSION=`pod --version`
-EARLIER_VERSION=`echo "$POD_VERSION $REQUIRED_POD_VERSION" | tr " " "\n" | sort -V | head -1`
-
-if [ "$EARLIER_VERSION" != "$REQUIRED_POD_VERSION" ]; then
-    echo "ERROR: This version of CocoaPods is unsupported. Current version is $POD_VERSION. Minimal required version is $REQUIRED_POD_VERSION."
-    echo "See update instructions at https://guides.cocoapods.org/using/getting-started.html#updating-cocoapods"
-    exit 1
-fi
-
 # Generate dummy framework bo be consumed by Xcode.
-"$KOTLIN_DIR/gradlew" -p "$KOTLIN_DIR" generateDummyFramework
-
-# Run CocoaPods to configure the Xcode project.
-pod --project-directory="$IOS_DIR" install
+"$KOTLIN_DIR/gradlew" -p "$KOTLIN_DIR" packForXcode
 
 # Run test with Xcode
-xcodebuild -workspace "$IOS_DIR/ios.xcworkspace" -scheme ios -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11,OS=13.3"
+xcodebuild -project "$IOS_DIR/ios.xcodeproj" -scheme ios -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11,OS=13.3"
