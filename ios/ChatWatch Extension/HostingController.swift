c24260296f7a9ac8a199fa4de0fbe3f209f902d6
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-02-08 10:57:10 -0300

    Add a watchOS Target

---
+++ ios/ChatWatch Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  ChatWatch Extension
+//
+//  Created by Felipe Costa on 08/02/20.
+//  Copyright © 2020 Felipe Costa. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
