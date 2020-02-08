d6e3061a5112c661a000d40241c5bc5722109b14
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-02-08 12:03:46 -0300

    Remove -sdk parameter from ios building

--- build-native-ios.sh
+++ build-native-ios.sh
@@ -11,4 +11,4 @@ IOS_DIR="$DIR/ios"
 "$KOTLIN_DIR/gradlew" -p "$KOTLIN_DIR" packForXcode
 
 # Run test with Xcode
-xcodebuild -project "$IOS_DIR/ios.xcodeproj" -scheme ios -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11,OS=13.3"
+xcodebuild -project "$IOS_DIR/ios.xcodeproj" -scheme ios -destination "platform=iOS Simulator,name=iPhone 11,OS=13.3"
