a1b0b949b1275649df3c19d29d9240235a49abc1
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-08-23 11:21:40 -0300

    Upgrade to Kotlin 1.4.0 :)

--- common/core/src/commonMain/kotlin/Message.kt
+++ common/core/src/commonMain/kotlin/Message.kt
@@ -1,12 +1,11 @@
 package com.github.felipehjcosta.chatapp
 
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
 @Serializable
 data class Message(val author: String, val message: String)
 
-fun Message.stringify(): String = Json(JsonConfiguration.Stable).stringify(Message.serializer(), this)
+fun Message.stringify(): String = Json.encodeToString(this)
 
-fun String.toMessage(): Message = Json(JsonConfiguration.Stable).parse(Message.serializer(), this)
+fun String.toMessage(): Message = Json.decodeFromString(this)
