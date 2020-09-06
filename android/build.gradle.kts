cccb638222ab1d4911983475ece558b2832c2d54
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-09-06 10:56:16 -0300

    Migrate gradle script to Kotlin DSL

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,95 +1,109 @@
-apply plugin: 'com.android.application'
-apply plugin: 'kotlin-android'
-apply plugin: "androidx.navigation.safeargs.kotlin"
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-ext {
-    CIBuild = System.getenv("CI") == "true"
-    // allows for -Dpre-dex=false to be set
-    preDexEnabled = "true".equals(System.getProperty("pre-dex", "true"))
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
+extra["CIBuild"] = System.getenv("CI") == "true"
+// allows for -Dpre-dex=false to be set
+extra["preDexEnabled"] = System.getProperty("pre-dex", "true") == "true"
+
 android {
-    compileSdkVersion 29
+    compileSdkVersion(29)
 
     defaultConfig {
-        applicationId "com.github.felipehjcosta.chatapp"
-        minSdkVersion 21
-        targetSdkVersion 29
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.github.felipehjcosta.chatapp"
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
 
-        testInstrumentationRunner "android.support.test.runner.AndroidJUnitRunner"
-
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        debug {
-            buildConfigField 'String', 'CHAT_URL_SERVER', "\"ws://${getIP()}:8089\""
+        getByName("debug") {
+            buildConfigField("String", "CHAT_URL_SERVER", "\"ws://10.0.2.2:8089\"")
         }
-        release {
-            buildConfigField 'String', 'CHAT_URL_SERVER', "\"ws://${getIP()}:8089\""
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android.txt'), 'proguard-rules.pro'
+        getByName("release") {
+            buildConfigField("String", "CHAT_URL_SERVER", "\"ws://10.0.2.2:8089\"")
+            isMinifyEnabled = true
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro"
+            )
         }
     }
 
     viewBinding {
-        enabled = true
+        isEnabled = true
     }
 
     dexOptions {
         // Skip pre-dexing when running on CI or when disabled via -Dpre-dex=false.
+        val preDexEnabled = extra["preDexEnabled"] as Boolean
+        val CIBuild = extra["preDexEnabled"] as Boolean
+
         preDexLibraries = preDexEnabled && !CIBuild
     }
 
-    packagingOptions {
-        exclude 'META-INF/kotlinx-serialization-runtime.kotlin_module'
+    dexOptions {
+
     }
 
-    sourceSets {
-        androidTest.java.srcDirs += "src/androidTest/kotlin"
-        debug.java.srcDirs += "src/debug/kotlin"
-        main.java.srcDirs += "src/main/kotlin"
-        test.java.srcDirs += "src/test/kotlin"
+    packagingOptions {
+        exclude("META-INF/kotlinx-serialization-runtime.kotlin_module")
     }
+
+    sourceSets.getByName("main").java.srcDir("src/main/kotlin")
+    sourceSets.getByName("test").java.srcDir("src/test/kotlin")
+    sourceSets.getByName("debug").java.srcDir("src/debug/kotlin")
+    sourceSets.getByName("androidTest").java.srcDir("src/androidTest/kotlin")
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
 }
 
-tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).all {
-    kotlinOptions {
-        allWarningsAsErrors = true
-        jvmTarget = "1.8"
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            allWarningsAsErrors = true
+            jvmTarget = "1.8"
+        }
     }
 }
+
+val kotlin_version: String by project
+val android_nav_version: String by project
+val android_jetpack_lifecycle: String by project
+val junit_version: String by project
 
 
 dependencies {
-    implementation fileTree(dir: 'libs', include: ['*.jar'])
-    implementation project(":common:client")
-    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
-    implementation "androidx.appcompat:appcompat:1.1.0"
-    implementation "androidx.recyclerview:recyclerview:1.1.0"
-    implementation 'com.google.android.material:material:1.0.0'
-    implementation 'com.kirich1409.viewbindingpropertydelegate:viewbindingpropertydelegate:1.0.0'
-    implementation "com.github.felipehjcosta:recyclerview-dsl:0.8.0"
+    implementation(project(":common:client"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("com.kirich1409.viewbindingpropertydelegate:viewbindingpropertydelegate:1.0.0")
+    implementation("com.github.felipehjcosta:recyclerview-dsl:0.8.0")
 
-    implementation "androidx.navigation:navigation-fragment-ktx:$android_nav_version"
-    implementation "androidx.navigation:navigation-ui-ktx:$android_nav_version"
+    implementation("androidx.navigation:navigation-fragment-ktx:$android_nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$android_nav_version")
 
-    implementation "androidx.lifecycle:lifecycle-runtime:$android_jetpack_lifecycle"
-    implementation "androidx.lifecycle:lifecycle-common-java8:$android_jetpack_lifecycle"
-    implementation "androidx.lifecycle:lifecycle-viewmodel-ktx:$android_jetpack_lifecycle"
-    implementation "androidx.lifecycle:lifecycle-livedata-ktx:$android_jetpack_lifecycle"
+    implementation("androidx.lifecycle:lifecycle-runtime:$android_jetpack_lifecycle")
+    implementation("androidx.lifecycle:lifecycle-common-java8:$android_jetpack_lifecycle")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$android_jetpack_lifecycle")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$android_jetpack_lifecycle")
 
-    implementation 'androidx.legacy:legacy-support-v4:1.0.0'
-    testImplementation "junit:junit:$junit_version"
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    testImplementation("junit:junit:$junit_version")
 
-    androidTestImplementation 'androidx.test:runner:1.2.0'
-    androidTestImplementation 'androidx.test.ext:junit:1.1.1'
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.2.0'
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
-rootProject.name = 'chatapp'
-include ':common:core', ':common:logger', ':common:client', ':common:client-websocket'
-include ':backend'
-include ':frontend'
-include ':android'
+rootProject.name = "chatapp"
+include(":common:core", ":common:logger", ":common:client", ":common:client-websocket")
+include(":backend")
+include(":frontend")
+include(":android")
 
