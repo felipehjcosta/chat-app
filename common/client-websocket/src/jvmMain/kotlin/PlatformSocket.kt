b4dee26499b171e61993935633041556426f63f8
felipehjcosta <felipehjcosta@pm.me>
2021-06-01 09:08:00 -0300

    Fix warning of GlobalScope delicate API

--- common/client-websocket/src/jvmMain/kotlin/PlatformSocket.kt
+++ common/client-websocket/src/jvmMain/kotlin/PlatformSocket.kt
@@ -1,9 +1,7 @@
 package com.github.felipehjcosta.chatapp.client
 
 import com.github.felipehjcosta.chatapp.logging.Logger
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -14,6 +12,8 @@ actual open class PlatformSocket actual constructor(url: String) {
     private val socketEndpoint = url
     private var webSocket: WebSocket? = null
 
+    private val mainCoroutineScope = CoroutineScope(Dispatchers.Main)
+
     actual open fun openSocket(listener: PlatformSocketListener) {
         val socketRequest = Request.Builder().url(socketEndpoint).build()
         val webClient = OkHttpClient().newBuilder().build()
@@ -22,40 +22,44 @@ actual open class PlatformSocket actual constructor(url: String) {
             object : okhttp3.WebSocketListener() {
                 override fun onOpen(webSocket: WebSocket, response: Response) {
                     Logger.info("WebSocketListener::onOpen with response: ${response}")
-                    GlobalScope.launch(Dispatchers.Main) {
+                    runOnMainThread {
                         listener.onOpen()
                     }
                 }
 
                 override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
                     Logger.info("WebSocketListener::onFailure with Throwable: ${t} and response: ${response}")
-                    GlobalScope.launch(Dispatchers.Main) {
+                    runOnMainThread {
                         listener.onFailure(t)
                     }
                 }
 
                 override fun onMessage(webSocket: WebSocket, text: String) {
                     Logger.info("WebSocketListener::onMessage with text: ${text}")
-                    GlobalScope.launch(Dispatchers.Main) {
+                    runOnMainThread {
                         listener.onMessage(text)
                     }
                 }
 
                 override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
                     Logger.info("WebSocketListener::onClosing with code: ${code} and reason: ${reason}")
-                    GlobalScope.launch(Dispatchers.Main) {
+                    runOnMainThread {
                         listener.onClosing(code, reason)
                     }
                 }
 
                 override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
                     Logger.info("WebSocketListener::onClosed with code: ${code} and reason: ${reason}")
-                    GlobalScope.launch(Dispatchers.Main) {
+                    runOnMainThread {
                         listener.onClosed(code, reason)
                     }
                 }
             }
         )
+    }
+
+    private fun runOnMainThread(block : ()-> Unit) {
+        mainCoroutineScope.launch { block() }
     }
 
     actual open fun closeSocket(code: Int, reason: String) {
